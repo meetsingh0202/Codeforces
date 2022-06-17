47a11df679d60fdd8b7c324963178fc0085bdc25
Meet Singh <97513967+meetsingh0202@users.noreply.github.com>
2022-06-17 19:17:06 +0530

    Add files via upload

---
+++ A_Captain_Flint_and_Crew_Recruitment.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    n=int(input())
+    if n<=30:
+        print("NO")
+    else:
+        print("YES")
+        if n== 36 or n == 40 or n == 44:
+            print(6,10,15,n-31)
+        else:
+            print(6,10,14,n-30)
+    
